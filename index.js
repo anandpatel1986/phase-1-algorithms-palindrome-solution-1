3e76f2cea47e8d75ce7f78fb7ee2ee88f17c180f
Anandkumar Patel <avpatel2005@gmail.com>
2023-04-19 22:14:37 -0400

    solution 1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word)
+  return word === reversedWord
 }
 
+function reverse(word){
+  return word.split("").reverse().join("")
+}
 /* 
   Add your pseudocode here
 */
